45359af01c673059290ef71509eb8715a83a02cb
C410 <C410@AceLabs.lagcc>
2017-06-08 15:16:18 -0400

    updated css and js to allow list to add on top

--- js/app.js
+++ js/app.js
@@ -9,9 +9,28 @@ function addToDo(event){
 		return false;
 	}
 
-	toDoList.innerHTML += '<li>' + userInput.value + '</li>';
-
+	toDoList.innerHTML = '<li>' + userInput.value + '</li>'+ toDoList.innerHTML;
 	userInput.value = '';
 }
 
 userSubmit.addEventListener('click', addToDo, false);
+
+// var myNodelist = document.getElementsByTagName("<li>");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
